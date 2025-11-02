29b0c9c9fa30e1aa863151dd858551ad69d954f9
Urdatorn <urdatorn@hotmail.com>
2025-11-02 11:15:58 +0100

    new plot options to jekyll

--- compiler.py
+++ compiler.py
@@ -12,5 +12,5 @@ print("                ||     || ")
 print("\n")
 
 #for odes in ["olympians", "pythians", "nemeans", "isthmians"]:
-for odes in ["olympians"]:
-    process_file(f"data/scan/ht_{odes}_strophes.xml", f"data/compiled/strophes/ht_{odes}_strophes.xml")
+for odes in [["olympians", "triads"]]:
+    process_file(f"data/scan/ht_{odes[0]}_{odes[1]}_corrected.xml", f"data/compiled/{odes[1]}/ht_{odes[0]}_{odes[1]}.xml")
